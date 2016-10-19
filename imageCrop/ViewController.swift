15f55cbc0ebf97a68ad13fb5f2a03788948902ed
Shinya Yamamoto <yamashin1003@hotmail.co.jp>
2016-10-19 13:16:24 +0900

    WIP

--- imageCrop/ViewController.swift
+++ imageCrop/ViewController.swift
@@ -31,23 +31,26 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
     @IBAction func tapResetButton(_ sender: AnyObject) {
         imageView.frame = originalFrame
         imageView.image = originalImage
-        self.imageViewSizeToFit()
+        self.imageView.isHidden = true
+        cView.isHidden = false
+        cView.resetViews()
     }
     
     //切り取り処理
     //切り取った後、imageViewのframeを更新
-    @IBAction func cropImage(_ sender: AnyObject) {
-        let scale = imageView.image!.size.width / imageView.frame.size.width
-        let rect = CGRect(x: cView.crop.frame.origin.x * scale,
-                          y: cView.crop.frame.origin.y * scale,
-                          width: cView.crop.frame.size.width * scale,
-                          height: cView.crop.frame.size.height * scale)
-        let imageRef:CGImage = imageView.image!.cgImage!.cropping(to: rect)!
-        let croppedImage:UIImage = UIImage(cgImage: imageRef)
-        imageView.image = croppedImage
+    @IBAction func cropImage(_ sender: UIButton) {
         
-        print(croppedImage)
-        self.imageViewSizeToFit()
+        if sender.currentTitle == "切り取り" {
+            self.imageView.image = cView.passCroppedImage()
+            self.imageView.isHidden = false
+            cView.isHidden = true
+            sender.setTitle("切り取り開始", for: .normal)
+        } else {
+            self.imageView.isHidden = true
+            self.cView.isHidden = false
+            sender.setTitle("切り取り", for: .normal)
+        }
+        
         
     }
     
@@ -65,144 +68,26 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         imageView.backgroundColor = UIColor.red
-        originalFrame = imageView.frame
+        
         
         //ImageViewリサイズ
         imageView.translatesAutoresizingMaskIntoConstraints = true
         outerView.translatesAutoresizingMaskIntoConstraints = true
-        
         self.imageViewSizeToFit()
-        
+        originalFrame = imageView.frame
         
         
         cView = cropView.instanceFromNib() as! cropView
-        cView.backgroundColor = UIColor.clear
+        cView.imageView.image = self.imageView.image
         cView.frame = imageView.frame
-        cView.crop.alpha = 0.5
-        cView.LAView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(panViewGesture(_:))))
-        cView.RAView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(panViewGesture(_:))))
-        cView.LBView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(panViewGesture(_:))))
-        cView.RBView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(panViewGesture(_:))))
         self.outerView.addSubview(cView)
-        cView.layoutIfNeeded()
-        cView.resize()
+        
+        self.imageView.isHidden = true
         
         
         
     }
     
-    func panViewGesture(_ sender: UIPanGestureRecognizer) {
-        
-        let translation = sender.translation(in: sender.view)
-        sender.view!.center = CGPoint(x: sender.view!.center.x + translation.x, y: sender.view!.center.y + translation.y)
-        sender.setTranslation(CGPoint.zero, in: self.view)
-        
-        print("\(cView.crop.frame.width)" + "¥" + "\(outerView.frame.width)")
-        
-        
-        //正方形リターン
-        if (cView.crop.frame.maxX > outerView.frame.width  ||
-            cView.crop.frame.maxY > outerView.frame.height ||
-            cView.crop.frame.minY < 0 ||
-            cView.crop.frame.minX < 0 ){
-            cView.crop.frame = CGRect(x: 0, y: 0, width: outerView.frame.width, height: outerView.frame.height)
-            return
-        }
-        
-        //右下リターン
-        if cView.RBView.frame.maxX > outerView.frame.width {
-           cView.RBView.frame.origin.x = outerView.frame.width - cView.RBView.frame.width
-            return
-        }
-
-        if cView.RBView.frame.maxY > outerView.frame.height {
-           cView.RBView.frame.origin.y = outerView.frame.height - cView.RBView.frame.height
-           return
-        }
-        
-        //右上リターン
-        if cView.RAView.frame.maxX > outerView.frame.width {
-            cView.RAView.frame.origin.x = outerView.frame.width - cView.RAView.frame.width
-            return
-        }
-        
-        if cView.RAView.frame.minY < 0 {
-            cView.RAView.frame.origin.y = 0
-            return
-        }
-        
-        //左上リターン
-        if cView.LAView.frame.minX < 0 {
-            cView.LAView.frame.origin.x = 0
-            return
-        }
-        
-        if cView.LAView.frame.minY < 0 {
-            cView.LAView.frame.origin.y = 0
-            return
-        }
-        
-        //左下リターン
-        if cView.LBView.frame.minX < 0 {
-            cView.LBView.frame.origin.x = 0
-            return
-        }
-        
-        if cView.LBView.frame.maxY > outerView.frame.height {
-            cView.LBView.frame.origin.y = outerView.frame.height - cView.LBView.frame.height
-            return
-        }
-        
-        
-        
-        
-        if (sender.view == cView.RBView) {
-            
-            cView.RAView.center.x = (sender.view?.center.x)!
-            cView.LBView.center.y = (sender.view?.center.y)!
-        }
-        else if (sender.view == cView.RAView) {
-            cView.RBView.center.x = (sender.view?.center.x)!
-            cView.LAView.center.y = (sender.view?.center.y)!
-        }
-        else if (sender.view == cView.LAView) {
-            cView.LBView.center.x = (sender.view?.center.x)!
-            cView.RAView.center.y = (sender.view?.center.y)!
-        }
-        else if (sender.view == cView.LBView) {
-            cView.LAView.center.x = (sender.view?.center.x)!
-            cView.RBView.center.y = (sender.view?.center.y)!
-        }
-            
-        cView.crop.frame = CGRect(x: cView.LAView.frame.minX,
-                                  y: cView.LAView.frame.minY,
-                                  width: cView.RAView.frame.maxX - cView.LAView.frame.minX,
-                                  height: cView.RBView.frame.maxY - cView.RAView.frame.minY)
-    }
-    
-    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
-        return true
-    }
-    
-    
-    
-//    - (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event
-//    {
-//    // "hidden", "userInteractionEnabled", "alpha" の値を考慮する
-//    if (self.button.isHidden || self.button.userInteractionEnabled == NO || self.button.alpha < 0.01) {
-//    return [super hitTest:point withEvent:event];
-//    }
-//    
-//    // button のあたり判定 rect を作成
-//    CGRect rect = CGRectInset(self.button.frame, -20, -10);
-//    // あたり判定 rect 内であれば、button を返し、button にイベントを受けられるようにする
-//    if (CGRectContainsPoint(rect, point)) {
-//    return self.button;
-//    }
-//    
-//    // button のあたり判定外だったら従来の挙動に任せる
-//    return [super hitTest:point withEvent:event];
-//    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
--- imageCrop/cropView.swift
+++ imageCrop/cropView.swift
@@ -14,6 +14,8 @@ class cropView :UIView {
     @IBOutlet weak var RAView:UIView!
     @IBOutlet weak var LBView:UIView!
     @IBOutlet weak var RBView:UIView!
+    @IBOutlet weak var imageView: UIImageView!
+    var originalImage: UIImage!
     
     class func instanceFromNib() -> UIView {
         return UINib(nibName: "cropView", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as! UIView
@@ -22,13 +24,154 @@ class cropView :UIView {
     override func awakeFromNib() {
         super.awakeFromNib()
         
+        self.backgroundColor = UIColor.clear
+        self.crop.alpha = 0.5
+        self.crop.layer.borderColor = UIColor.blue.cgColor
+        self.crop.layer.borderWidth = 2.0
+        self.LAView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(panViewGesture(_:))))
+        self.RAView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(panViewGesture(_:))))
+        self.LBView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(panViewGesture(_:))))
+        self.RBView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(panViewGesture(_:))))
+        
     }
     
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        imageView.frame = self.bounds
+    }
     
-    func resize() {
-        self.crop.frame = CGRect(x: self.LAView.frame.minX,
-                                 y: self.LAView.frame.minY,
-                                 width: self.RAView.frame.maxX - self.LAView.frame.minX,
-                                 height: self.RBView.frame.maxY - self.RAView.frame.minY)
+    func passCroppedImage() -> UIImage {
+        let scale = imageView.image!.size.width / imageView.frame.size.width
+        let rect = CGRect(x: self.crop.frame.origin.x * scale,
+                          y: self.crop.frame.origin.y * scale,
+                          width: self.crop.frame.size.width * scale,
+                          height: self.crop.frame.size.height * scale)
+        let imageRef:CGImage = imageView.image!.cgImage!.cropping(to: rect)!
+        let croppedImage:UIImage = UIImage(cgImage: imageRef)
+        print(croppedImage)
+        return croppedImage
+    }
+    
+//    func imageViewSizeToFit() {
+//        
+//        if imageView.image!.size.width > imageView.image!.size.height {
+//            imageView.frame.size.height = imageView.image!.size.height * imageView.frame.size.width / imageView.image!.size.width
+//        } else {
+//            imageView.frame.size.width = imageView.image!.size.width * imageView.frame.size.height / imageView.image!.size.height
+//        }
+//        self.frame = imageView.bounds
+//    }
+    
+//    func resize() {
+//        self.crop.frame = CGRect(x: self.LAView.frame.minX,
+//                                 y: self.LAView.frame.minY,
+//                                 width: self.RAView.frame.maxX - self.LAView.frame.minX,
+//                                 height: self.RBView.frame.maxY - self.RAView.frame.minY)
+//    }
+    
+    func resetViews() {
+        self.crop.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height)
+        self.RBView.frame.origin.x = self.frame.width - self.RBView.frame.width
+        self.RBView.frame.origin.y = self.frame.height - self.RBView.frame.height
+        self.RAView.frame.origin.x = self.frame.width - self.RAView.frame.width
+        self.RAView.frame.origin.y = 0
+        self.LAView.frame.origin.x = 0
+        self.LAView.frame.origin.y = 0
+        self.LBView.frame.origin.x = 0
+        self.LBView.frame.origin.y = self.frame.height - self.LBView.frame.height
+    }
+    
+    func panViewGesture(_ sender: UIPanGestureRecognizer) {
+        
+        let translation = sender.translation(in: sender.view)
+        sender.view!.center = CGPoint(x: sender.view!.center.x + translation.x, y: sender.view!.center.y + translation.y)
+        sender.setTranslation(CGPoint.zero, in: self)
+        
+        print("\(self.crop.frame.width)" + "¥" + "\(self.frame.width)")
+        
+        
+        //正方形リターン
+        if (self.crop.frame.maxX > self.frame.width  ||
+            self.crop.frame.maxY > self.frame.height ||
+            self.crop.frame.minY < 0 ||
+            self.crop.frame.minX < 0 ){
+            self.crop.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height)
+            return
+        }
+        
+        //右下リターン
+        if self.RBView.frame.maxX > self.frame.width {
+            self.RBView.frame.origin.x = self.frame.width - self.RBView.frame.width
+            return
+        }
+        
+        if self.RBView.frame.maxY > self.frame.height {
+            self.RBView.frame.origin.y = self.frame.height - self.RBView.frame.height
+            return
+        }
+        
+        //右上リターン
+        if self.RAView.frame.maxX > self.frame.width {
+            self.RAView.frame.origin.x = self.frame.width - self.RAView.frame.width
+            return
+        }
+        
+        if self.RAView.frame.minY < 0 {
+            self.RAView.frame.origin.y = 0
+            return
+        }
+        
+        //左上リターン
+        if self.LAView.frame.minX < 0 {
+            self.LAView.frame.origin.x = 0
+            return
+        }
+        
+        if self.LAView.frame.minY < 0 {
+            self.LAView.frame.origin.y = 0
+            return
+        }
+        
+        //左下リターン
+        if self.LBView.frame.minX < 0 {
+            self.LBView.frame.origin.x = 0
+            return
+        }
+        
+        if self.LBView.frame.maxY > self.frame.height {
+            self.LBView.frame.origin.y = self.frame.height - self.LBView.frame.height
+            return
+        }
+        
+        
+        
+        
+        if (sender.view == self.RBView) {
+            
+            self.RAView.center.x = (sender.view?.center.x)!
+            self.LBView.center.y = (sender.view?.center.y)!
+        }
+        else if (sender.view == self.RAView) {
+            self.RBView.center.x = (sender.view?.center.x)!
+            self.LAView.center.y = (sender.view?.center.y)!
+        }
+        else if (sender.view == self.LAView) {
+            self.LBView.center.x = (sender.view?.center.x)!
+            self.RAView.center.y = (sender.view?.center.y)!
+        }
+        else if (sender.view == self.LBView) {
+            self.LAView.center.x = (sender.view?.center.x)!
+            self.RBView.center.y = (sender.view?.center.y)!
+        }
+        
+        self.crop.frame = CGRect(x: self.LAView.frame.minX
+            ,
+                                  y: self.LAView.frame.minY,
+                                  width: self.RAView.frame.maxX - self.LAView.frame.minX,
+                                  height: self.RBView.frame.maxY - self.RAView.frame.minY)
+    }
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
     }
 }
